3acad065aa270b1594314b5200569236372547d3
AbeerAl-Rafati <Abeerw.rafati@yahoo.com>
2021-05-06 14:38:15 +0300

    calss8 seems as class6 xD

--- src/WeatherA.js
+++ src/WeatherA.js
@@ -1,29 +1,3 @@
-// import React from 'react';
-
-
-
-// class WeatherA extends React.Component {
-//   render() {
-
-//     return (
-//       this.props.weatherInfo.map((item, index) => {
-//         return (
-//           <div key={index}>
-//             <p>In day  : {item.date}</p>
-//             <p>The weather in the city : {item.status}</p>
-//             <p>========================</p>
-//           </div>
-//         );
-
-
-//       })
-//     );
-//   }
-
-// }
-
-// export default WeatherA;
-
 import React from 'react';
 
 
@@ -32,19 +6,22 @@ class WeatherA extends React.Component {
   render() {
 
     return (
+      // console.log(this.props.weatherInfo),
+      this.props.weatherInfo.map((item, index) => {
+        return (
+          <div key={index}>
+            <p>In day  : {item.date}</p>
+            <p>The weather in the city : {item.status}</p>
+            <p>========================</p>
+          </div>
+        );
 
-      <div>
-        <p>In day  : {this.props.weatherInfo}</p>
-        <p>The weather in the city :  {this.props.weatherInfo}</p>
-        <p>========================</p>
-        console.log({this.props.weatherInfo});
-      </div>
+
+      })
     );
-
-
-
   }
 
 }
 
 export default WeatherA;
+
